bdc9b4c4d00ef10207a8fbfc910bc40d470565ee
Tim <tims1334@gmail.com>
2022-08-31 10:59:30 -0400

    Updating a few things, adding tabs

--- nfl_pbp.py
+++ nfl_pbp.py
@@ -3,43 +3,58 @@ import pandas as pd
 import streamlit as st
 import altair as alt
 
-ay_max = st.sidebar.number_input('max air yards', min_value=-10, max_value=80, value=20)
-ay_min = st.sidebar.number_input('min air yards', min_value=-10, max_value=80, value=5)
-min_att = st.sidebar.number_input('Minimum number of attempts', min_value=1, max_value=2000, value = 15)
-data = nfl.import_pbp_data(years=[2020,2021],downcast=False,cache=False)
+st.sidebar.header('Choose Seasons')
+year_min = st.sidebar.number_input('First Season',min_value=2018, max_value=2022,value=2021)
+year_max = st.sidebar.number_input('Last Season',min_value=2018, max_value=2022,value=2021)
+chosen_years = []
+chosen_years.extend(range(int(year_min), int(year_max)+1))
+st.sidebar.header('Choose Air Yards Range')
+ay_max = st.sidebar.number_input('Max Air Yards', min_value=-10, max_value=80, value=20)
+ay_min = st.sidebar.number_input('Min Air Yards', min_value=-10, max_value=80, value=5)
+min_att = st.sidebar.number_input('Minimum Number of Attempts', min_value=1, max_value=2000, value = 15)
+binsize = st.sidebar.number_input('Bin Size', min_value=5, max_value=100, value = 20)
+
+@st.cache
+def get_data():
+    data = nfl.import_pbp_data(years=chosen_years,downcast=False,cache=False)
+    return data
+
+data = get_data()
 
 qbs = data.drop_duplicates('passer')
 qb_list = qbs.passer.to_list()
 qb_options = st.sidebar.multiselect('Pick QBs to compare',qb_list,default='T.Tagovailoa')
 passes = data.loc[data.passer.isin(qb_options)]
 
-# Group passers, mean stats
-full_mean = passes.groupby('passer').mean()
+# Group passers
 
 air_passes = passes[['air_yards','air_wpa','air_epa','yards_gained','cp','cpoe','epa','passer','pass_touchdown','interception']]
 air_passes = air_passes.loc[(air_passes['air_yards']>=ay_min)&(air_passes['air_yards']<=ay_max)]
 metrics = air_passes.columns.to_list()
-
-st.header('Averages for each stat in NFLfastR')
-st.dataframe(full_mean)
-
-
 metric = st.sidebar.selectbox('Pick Metric',metrics)
 
-#for i in range(len(qb_options)):
-#    df[qb_options[i]] = air_passes.loc[air_passes['air_yards']==qb_options[i]]
+### Layouts
+tab1, tab2, tab3 = st.tabs(["QB Charts","QB Table","WR Charts"])
+col1, col2 = st.columns(2)
 
-c= alt.Chart(air_passes[['passer',metric]]).mark_bar(
+### Passer Charts
+tab1.header("QB Charts")
+
+c = alt.Chart(air_passes[['passer',metric]]).mark_bar(
 opacity=0.3,
 binSpacing=0
 ).encode(
-alt.X(metric, bin=alt.Bin(maxbins=20)),
+alt.X(metric, bin=alt.Bin(maxbins=binsize)),
     alt.Y('count()',
     stack=None),
     alt.Color('passer'))
-st.header('Counts')
-st.altair_chart(c, use_container_width=True)
-st.header('Percentage')
+
+### Counts
+tab1.header('Counts')
+tab1.altair_chart(c, use_container_width=True)
+
+### Percentages
+tab1.header('Percentage')
 cb = alt.Chart(air_passes[['passer',metric]]).transform_joinaggregate(
     total='count(*)',
     groupby=['passer']
@@ -49,13 +64,16 @@ cb = alt.Chart(air_passes[['passer',metric]]).transform_joinaggregate(
 opacity=0.3,
 binSpacing=0
 ).encode(
-    alt.X(metric, bin=alt.Bin(maxbins=20)),
+    alt.X(metric, bin=alt.Bin(maxbins=binsize)),
     alt.Y('sum(pct):Q', axis=alt.Axis(format='%'),stack=None),
     alt.Color('passer'))
-st.altair_chart(cb, use_container_width=True)
+tab1.altair_chart(cb, use_container_width=True)
+
+
+### WRs
+tab3.header("WR Charts")
 
 wrs = data.loc[(data['pass_attempt']==1)]
-st.write(len(wrs))
 
 wr_stats = ['receiver','receiving_yards','touchdown',
             'xyac_epa','xyac_mean_yardage','yac_epa','yards_after_catch',
@@ -77,17 +95,19 @@ metrics_w = st.sidebar.selectbox('Pick Metric',metrics_w)
 
 wrsdf = wrsdf.loc[wrsdf['receiver'].isin(wr_selections)]
 
-cw= alt.Chart(wrsdf[['receiver',metrics_w]]).mark_bar(
+cw = alt.Chart(wrsdf[['receiver',metrics_w]]).mark_bar(
 opacity=0.3,
 binSpacing=0
 ).encode(
-alt.X(metrics_w, bin=alt.Bin(maxbins=100)),
+alt.X(metrics_w, bin=alt.Bin(maxbins=binsize)),
     alt.Y('count()',
     stack=None),
     alt.Color('receiver'))
-st.header('Counts')
-st.altair_chart(cw, use_container_width=True)
-st.header('Percentage')
+### Counts
+tab3.subheader('Counts')
+tab3.altair_chart(cw, use_container_width=True)
+### Percentages
+tab3.subheader('Percentage')
 cbw = alt.Chart(wrsdf[['receiver',metrics_w]]).transform_joinaggregate(
     total='count(*)',
     groupby=['receiver']
@@ -97,12 +117,13 @@ cbw = alt.Chart(wrsdf[['receiver',metrics_w]]).transform_joinaggregate(
 opacity=0.3,
 binSpacing=0
 ).encode(
-    alt.X(metrics_w, bin=alt.Bin(maxbins=50)),
+    alt.X(metrics_w, bin=alt.Bin(maxbins=binsize)),
     alt.Y('sum(pct):Q', axis=alt.Axis(format='%'),stack=None),
     alt.Color('receiver'))
-st.altair_chart(cbw, use_container_width=True)
+tab3.altair_chart(cbw, use_container_width=True)
 
-
+### QB Table
+tab2.header("QB Table")
 qbdf = data.loc[(data['air_yards'] >= ay_min) & (data['air_yards'] <= ay_max)]
 qbdf['plays'] = 1
 qbdf = qbdf.groupby(['passer']).agg(mean_epa=('epa','mean'),
@@ -111,6 +132,8 @@ qbdf = qbdf.groupby(['passer']).agg(mean_epa=('epa','mean'),
                                     total_air_epa=('air_epa','sum'),
                                     ints = ('interception','sum'),
                                     tds = ('pass_touchdown','sum'),
+                                    completion_probability_mean = ('cp','mean'),
+                                    completion_percent_over_expected_mean = ('cpoe','mean'),
                                     total_plays=('plays','sum')).reset_index()
 qbdf = qbdf.loc[qbdf['total_plays']>=min_att]
-st.dataframe(qbdf.sort_values('total_plays', ascending=False))
+tab2.dataframe(qbdf.sort_values('total_plays', ascending=False))
